dda0b6555bc497010e87d5ee7caacbe691d489be
Divya Prakash <95012051+divyaprakash-programmer@users.noreply.github.com>
2022-07-31 16:25:34 +0530

    Add files via upload

---
+++ array_is_sorted_or_not.java
@@ -0,0 +1,27 @@
+package com.divya;   // you can put here your package name
+
+public class array_is_sorted_or_not {
+    public static void main(String[] args) {
+        System.out.println("A java program to check whether the array is sorted or not.");
+
+        boolean isSorted = true;         //initialise to true
+        int [] arr = {1,2,3, 4,4,5,5,34,67};
+        for(int i=0; i < arr.length-1; i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+               isSorted = false;
+               break;
+            }
+        }
+
+        if(isSorted)
+        {
+            System.out.println("Sorted array");
+        }
+        else{
+            System.out.println("Unsorted array");
+        }
+
+    }
+}
